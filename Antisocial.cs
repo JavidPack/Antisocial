8e966dc648ca7ee296f13bf0ece45db132cf3bd7
JavidPack <javidpack@gmail.com>
2018-09-26 22:45:00 -0600

    v1.1, now displays stats in social slots.

--- Antisocial.cs
+++ Antisocial.cs
@@ -1,10 +1,35 @@
-﻿using System.Collections.Generic;
+﻿using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
 using Terraria;
 using Terraria.ModLoader;
 
 namespace Antisocial
 {
-	public class Antisocial : Mod { }
+	public class Antisocial : Mod
+	{
+		const string HarmonyID = "mod.Antisocial";
+		HarmonyInstance harmonyInstance;
+
+		public override void Load()
+		{
+			harmonyInstance = HarmonyInstance.Create(HarmonyID);
+			if (!harmonyInstance.HasAnyPatches(HarmonyID)) // In case Unload failed, don't double up.
+			{
+				harmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
+			}
+		}
+
+		public override void Unload()
+		{
+			if (harmonyInstance != null)
+			{
+				harmonyInstance.UnpatchAll(HarmonyID);
+			}
+		}
+	}
 
 	public class AntisocialPlayer : ModPlayer
 	{
@@ -46,9 +71,52 @@ namespace Antisocial
 		{
 			if (item.social && item.accessory)
 			{
-				tooltips.RemoveAll(x => x.Name == "Social" || x.Name == "SocialDesc");
-				tooltips.Add(new TooltipLine(mod, "SocialCheat", "Antisocial: Stats WILL be gained, but are not shown"));
+				//tooltips.RemoveAll(x => x.Name == "Social" || x.Name == "SocialDesc");
+				tooltips.Add(new TooltipLine(mod, "SocialCheat", "Antisocial: Stats WILL be gained"));
 			}
+		}
+	}
+
+	[HarmonyPatch(typeof(Terraria.UI.ItemSlot))]
+	[HarmonyPatch("MouseHover")]
+	[HarmonyPatch(new Type[] { typeof(Item[]), typeof(int), typeof(int) })]
+	public class MouseText_DrawItemTooltip_Patcher
+	{
+		static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator il)
+		{
+			//	Modifies this code:
+			//	if (context == 11 || context == 9)
+			//	{
+			//		Main.HoverItem.social = true;
+			//		if (context == 11) // Only works on accessories for now.
+			//+			Main.HoverItem.social = false; // it is simpler to write this in IL.
+			//	}
+
+			bool success = false;
+			foreach (CodeInstruction instruction in instructions)
+			{
+				if (instruction.opcode == OpCodes.Stfld && ((FieldInfo)instruction.operand) == AccessTools.Field(typeof(Item), nameof
+					(Item.social)))
+				{
+					yield return instruction;
+					Label afterIf11 = il.DefineLabel();
+
+					yield return new CodeInstruction(OpCodes.Ldarg_1);
+					yield return new CodeInstruction(OpCodes.Ldc_I4_S, (byte)11);
+					yield return new CodeInstruction(OpCodes.Bne_Un_S, afterIf11);
+
+					yield return new CodeInstruction(OpCodes.Ldsfld, AccessTools.Field(typeof(Main), nameof(Main.HoverItem)));
+					yield return new CodeInstruction(OpCodes.Ldc_I4_0);
+					yield return new CodeInstruction(OpCodes.Stfld, AccessTools.Field(typeof(Item), nameof
+					(Item.social)));
+					yield return new CodeInstruction(OpCodes.Nop) { labels = new List<Label>() { afterIf11 } };
+					success = true;
+				}
+				else
+					yield return instruction;
+			}
+			if(!success)
+				ErrorLogger.Log($"Antisocial patch failure. Please report to website. {ModLoader.versionedName}, {ModLoader.compressedPlatformRepresentation}, Antisocial {ModLoader.GetMod("Antisocial")?.Version}");
 		}
 	}
 }
